ab10cc1112b11cb4227488dc55ddcd8359eb5de0
Ngô Trí Hải <24520436@gm.uit.edu.vn>
2025-12-06 07:32:34 +0700

    removeLine.cpp

--- removeLine.cpp
+++ removeLine.cpp
@@ -1,14 +1,22 @@
-void removeLine(){
-    int j;
-    for (int i = H-2; i >0 ; i-- ){
-        for (j = 0; j < W-1 ; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W-1){
-            for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
-            i++;
-            draw();
-            _sleep(200);
+void removeLine()
+
+{
+    for (int i = H - 2; i >= 1; i--)
+        {
+            bool full = true;
+            for (int j = 1; j < W - 1; j++)
+                if (board[i][j] == ' ')  
+                { 
+                    full = false; 
+                    break; 
+                }
+            if (full) 
+            {
+                for (int k = i; k > 0; k--)
+                    for (int j = 1; j < W - 1; j++)
+                        board[k][j] = board[k - 1][j];
+                i++;
+            }
         }
-    }
 }
+
